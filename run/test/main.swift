eeaeec7291b26b051e19ad9591ae018cf5a6623f
zhuxietong <zhuxietong@me.com>
2021-01-09 20:29:49 +0800

    5.3.2

--- run/test/main.swift
+++ run/test/main.swift
@@ -8,71 +8,10 @@
 import Foundation
 import Eelay
 
-public protocol NSJsonObj {
-    func jsonObj<T>() -> T?
-}
-
-
-public protocol NSJsonSerial:NSJsonObj{
-    associatedtype NSJson
-    var nsjson:NSJson{get}
-}
-extension Array:NSJsonSerial{
-  
-    
-    public func jsonObj<T>() -> T? {
-        return nsjson as? T
-    }
-    
-    public typealias NSJson = NSMutableArray
-    public var nsjson: NSJson{
-        get{
-            return self.mutable_array
-        }
-    }
-}
-extension Dictionary:NSJsonSerial{
-    
-    public func jsonObj<T>() -> T? {
-        return nsjson as? T
-    }
-    public typealias NSJson = NSMutableDictionary
-    public var nsjson: NSJson{
-        get{
-            return self.mutable_dictionary
-        }
-    }
-}
-extension NSDictionary:NSJsonSerial{
-    
-    public func jsonObj<T>() -> T? {
-        return nsjson as? T
-    }
-    public typealias NSJson = NSMutableDictionary
-    public var nsjson: NSJson{
-        get{
-            return self.mutable_dictionary
-        }
-    }
-}
-
-extension NSArray:NSJsonSerial{
-    
-    public func jsonObj<T>() -> T? {
-        return nsjson as? T
-    }
-    public typealias NSJson = NSMutableArray
-    public var nsjson: NSJson{
-        get{
-            return self.mutable_array
-        }
-    }
-}
-
 
 let a:Any = ["23","sa"]
 if let c = a as? NSJsonObj{
-    let o:NSMutableDictionary? = c.jsonObj()
+    let o:NSMutableArray? = c.jsonObj()
     print(o as Any)
 }
 
